1de9696fe4139c54e9c4779e357ea0daddaaa2f2
RedPlus <redplus@redplus.net>
2019-05-29 02:07:46 +0900

    초기 커밋.

---
+++ DotNetSale.Models.Categories/CategoryBase.cs
@@ -0,0 +1,18 @@
+﻿namespace DotNetSale.Models.Categories
+{
+    /// <summary>
+    /// 카테고리 모델 클래스
+    /// </summary>
+    public class CategoryBase
+    {
+        /// <summary>
+        /// 카테고리 고유 일련번호
+        /// </summary>
+        public int CategoryId { get; set; }
+
+        /// <summary>
+        /// 카테고리 이름
+        /// </summary>
+        public string CategoryName { get; set; }
+    }
+}
