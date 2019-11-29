ff7b78a4eb69e18b20919bff5ca1a6377517c724
WebCampKR <webcamp@outlook.kr>
2019-11-29 19:02:11 +0900

    AddVideoAsync: Videos 테이블에 데이터를 입력하는 비동기 메서드 생성

---
+++ VideoAppCore.Models/VideoRepositoryAdoNet.cs
@@ -0,0 +1,82 @@
+﻿// Install-Package System.Data.SqlClient
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace VideoAppCore.Models
+{
+    public class VideoRepositoryAdoNet
+    {
+        private readonly string _connectionString;
+
+        public VideoRepositoryAdoNet(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
+
+        public Video AddVideo(Video model)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                const string query = 
+                    "Insert Into Videos(Title, Url, Name, Company, CreatedBy) Values(@Title, @Url, @Name, @Company, @CreatedBy);";
+                SqlCommand cmd = new SqlCommand(query, con) { CommandType = CommandType.Text };
+
+                cmd.Parameters.AddWithValue("@Title", model.Title);
+                cmd.Parameters.AddWithValue("@Url", model.Url);
+                cmd.Parameters.AddWithValue("@Name", model.Name);
+                cmd.Parameters.AddWithValue("@Company", model.Company);
+                cmd.Parameters.AddWithValue("@CreatedBy", model.CreatedBy);
+
+                con.Open();
+                cmd.ExecuteNonQuery(); 
+                con.Close();
+            }
+
+            return model; 
+        }
+
+        public async Task<Video> AddVideoAsync(Video model)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                const string query =
+                    "Insert Into Videos(Title, Url, Name, Company, CreatedBy) Values(@Title, @Url, @Name, @Company, @CreatedBy);";
+                SqlCommand cmd = new SqlCommand(query, con) { CommandType = CommandType.Text };
+
+                cmd.Parameters.AddWithValue("@Title", model.Title);
+                cmd.Parameters.AddWithValue("@Url", model.Url);
+                cmd.Parameters.AddWithValue("@Name", model.Name);
+                cmd.Parameters.AddWithValue("@Company", model.Company);
+                cmd.Parameters.AddWithValue("@CreatedBy", model.CreatedBy);
+
+                con.Open();
+                await cmd.ExecuteNonQueryAsync();
+                con.Close();
+            }
+
+            return model;
+        }
+
+        public Video GetVideoById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Video> GetVideos()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveVideo(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Video UpdateVideo(Video model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
